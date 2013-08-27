28040694d217ceccae155d49a4a4165986460819
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2013-08-27 11:27:47 -0700

    Make it easier to get bootstrapped with this script

---
+++ reset.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+verbose=false
+
+while test $# != 0
+do
+    case "$1" in
+        "-v") verbose=true;;
+        "--verbose") verbose=true;;
+    esac
+    shift
+done
+
+run_cmd() {
+    if $verbose ; then
+        "$@"
+    else
+        "$@" >/dev/null 2>&1
+    fi
+}
+
+echo "Initializing repo"
+run_cmd git submodule update --init
+cordova >/dev/null 2>&1 || { echo >&2 "Error: Please install cordova CLI first (npm install -g cordova)"; exit 1; }
+echo "Adding platforms iOS and Android"
+run_cmd cordova platforms add ios android || { run_cmd cordova platforms rm ios android; run_cmd cordova platforms add ios android; }
+echo "Building apps for iOS and Android"
+run_cmd cordova build
